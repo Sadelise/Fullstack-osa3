f9327a40470fe468e98a6beb9af11b40afb1bc33
Sadelise <sharhio@hotmail.com>
2018-01-29 10:57:33 +0200

    3.3

--- index.js
+++ index.js
@@ -31,7 +31,15 @@ app.get('/', (req, res) => {
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
-
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
 app.get('/info', (req, res) => {
     const date = new Date()
     res.send('puhelinluettelossa ' + persons.length + ' henkilön tiedot <br><br>' + date)
