4b2776d5331b12b28d701b72717c6796661247d1
Sadelise <sharhio@hotmail.com>
2018-01-29 10:46:12 +0200

    3.2

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    const date = new Date()
+    res.send('puhelinluettelossa ' + persons.length + ' henkilön tiedot <br><br>' + date)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
