fb10751b40f307d82e3a86295d9ac18d0080e00c
Sadelise <sharhio@hotmail.com>
2018-02-04 17:41:14 +0200

    3.12

---
+++ mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+// korvaa url oman tietokantasi urlilla!
+const url = 'mongodb://auser:moresekred@ds119258.mlab.com:19258/agile-beach-puhelinluettelo'
+
+mongoose.connect(url)
+mongoose.Promise = global.Promise;
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+if (process.argv[2] && process.argv[3]) {
+  const newperson = new Person({
+    name: process.argv[2],
+    number: process.argv[3]
+  })
+  newperson
+    .save()
+    .then(response => {
+      console.log('lisätään henkilö', newperson.name, 'numero', newperson.number, 'luetteloon')
+      mongoose.connection.close()
+    })
+} else {
+  console.log('puhelinluettelo:')
+  Person
+    .find({})
+    .then(result => {
+      result.forEach(person => {
+        console.log(person.name, person.number)
+      })
+      mongoose.connection.close()
+    })
+}
+
+
